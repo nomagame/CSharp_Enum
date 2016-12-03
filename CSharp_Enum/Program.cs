23a0bcfadfeef33a86c535455980b335c8c57f7e
Noma <1181098979@qq.com>
2016-12-04 00:24:58 +0800

    CSharp_Enum
    CSharp_Enum C#枚举类型

---
+++ CSharp_Enum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CSharp_Enum
+{
+	/// <summary>
+	/// Week.2016.12.4
+	/// </summary>
+	//	定义枚举类型
+	enum week
+	{
+		Monday,		//周一
+		Tuesday,	//周二
+		Wednesday,	//周三
+		Thursday,	//周四
+		Friday,		//周五
+		Saturday,	//周六
+		Sunday		//周日
+	}
+
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			//枚举类型初始化
+			week day = week.Friday;
+			Console.WriteLine (day);
+			Console.ReadKey ();
+		}
+	}
+}
